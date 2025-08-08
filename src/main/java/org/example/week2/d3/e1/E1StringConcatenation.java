1b4bb29c3b64b970cf7b3ea5f99c454e40a2dd0c
Deepa Ojha <45989183+deepa77@users.noreply.github.com>
2025-08-08 15:55:04 -0400

    Update E1StringConcatenation.java

--- src/main/java/org/example/week2/d3/e1/E1StringConcatenation.java
+++ src/main/java/org/example/week2/d3/e1/E1StringConcatenation.java
@@ -3,14 +3,14 @@ package org.example.week2.d3.e1;
 public class E1StringConcatenation {
     public static void main(String[] args) {
         // Step 1: Declare the String variable and set it to "John"
-
+String firstName ="John";
 
         // Step 2: Declare the String variable and set it to "Smith"
 
-
+String lastName ="Smith";
         // Step 3: Print the concatenated name
 
-
+System.out.println(firstName +" " +lastName);
 
     }
 }
