521f366d91d6f6f41e04063be9ef5743222baa77
Deepa Ojha <45989183+deepa77@users.noreply.github.com>
2025-08-08 15:52:03 -0400

    Update E2StringConcatenation.java

--- src/main/java/org/example/week2/d3/e2/E2StringConcatenation.java
+++ src/main/java/org/example/week2/d3/e2/E2StringConcatenation.java
@@ -3,12 +3,14 @@ package org.example.week2.d3.e2;
 public class E2StringConcatenation {
     public static void main(String[] args) {
         // Step 1: Declare the String variable and set it to "Java"
-
+String language ="Java";
 
         // Step 2: Declare the String variable and set it to "programming language"
-
+String indication ="programming language";
 
         // Step 3: Print the concatenated string
+        
+        System.out.println(language + "is the #1 " + indication);
 
     }
 }
